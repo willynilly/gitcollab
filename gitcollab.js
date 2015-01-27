934e724c8297c49f6256ec525e8112647b808b57
Will Riley <will.riley@slalom.com>
2015-01-27 08:46:51 -0500

    Added measure of temporal spread between coauthoring.  Created a boxplot for that measure.

--- gitcollab.js
+++ gitcollab.js
@@ -26,7 +26,6 @@ function loadCommits(repoLogJsonUrls, onLoadCallback) {
 	});
 	
   $.when.apply($, requests).done(function() {
-    console.log('commits', commits);
 		onLoadCallback(commits);
   });
 }
@@ -34,10 +33,8 @@ function loadCommits(repoLogJsonUrls, onLoadCallback) {
 function normalizeCommitsForAuthorEmailAliases(authorEmailAliases, commits) {
 	_.forEach(commits, function(commit) {
 		var authorEmail = commit.author.email;
-		console.log(authorEmail);
 		if (authorEmail in authorEmailAliases) {
 			commit.author.email = authorEmailAliases[authorEmail];
-			console.log(commit);
 		}
 	});
 	return commits;
@@ -62,7 +59,6 @@ function getContributionStats(commits) {
 			result[repoUrl] = {};
 		}
 		contributionsForRepo = result[repoUrl];
-		
 		var authorName = commit.author.name;
 		var authorEmail = commit.author.email;
 		var commitDate = commit.author.date;
@@ -96,16 +92,33 @@ function getContributionStats(commits) {
 				date: commitDate
 			});
 		});
-		
 		return result;
 	}, {});
 }
 
+function getChangeHistoryForFile(repoUrl, fileName, contributionStats) {
+	var changeHistoryForFile = [];
+	if (repoUrl in contributionStats && fileName in contributionStats[repoUrl]) {
+		contributionsByAuthorEmail = contributionStats[repoUrl][fileName];
+		_.forEach(contributionsByAuthorEmail, function(contribution, authorEmail) {
+			var changeHistoryByAuthorEmail = _.map(contribution.changeHistory, function(change) {
+				change.authorEmail = authorEmail;
+				return change;
+			});
+			_.forEach(changeHistoryByAuthorEmail, function(change) {
+				changeHistoryForFile.push(change);
+			});
+		});
+		changeHistoryForFile = _.sortByAll(changeHistoryForFile, ['date', 'authorEmail']);
+	}
+	return changeHistoryForFile;
+}
+
 function getCoauthorshipStats(contributionStats) {
 	return _.reduce(contributionStats, function(result, contributionsByFile, repoUrl) {
-		
 		_.forEach(contributionsByFile, function(contributionsByFromAuthorEmail, fileName) {
 			var authorEmailsForFile = _.keys(contributionsByFromAuthorEmail);
+			var changeHistoryForFile = getChangeHistoryForFile(repoUrl, fileName, contributionStats);
 			_.forEach(contributionsByFromAuthorEmail, function(contribution, fromAuthorEmail) {
 				_.forEach(authorEmailsForFile, function(toAuthorEmail) {
 					if (toAuthorEmail !== fromAuthorEmail) {
@@ -116,22 +129,121 @@ function getCoauthorshipStats(contributionStats) {
 							result[fromAuthorEmail][toAuthorEmail] = {
 								totalFiles: 0, 
 								totalAddedLines: 0, 
-								totalRemovedLines: 0 
+								totalRemovedLines: 0,
+								timeGapHistoryForClosestChanges: [],
+								totalTimeBetweenClosestChanges: 0
 							};
 						}
 						var stats = result[fromAuthorEmail][toAuthorEmail];
 						stats.totalFiles += 1;
 						stats.totalAddedLines += contribution.totalAddedLines;
 						stats.totalRemovedLines += contribution.totalRemovedLines;
+
+						var newTimeGapHistory = getTimeGapHistoryForClosestChanges(fromAuthorEmail, toAuthorEmail, changeHistoryForFile);
+						stats.totalTimeBetweenClosestChanges += getTotalTimeBetweenClosestChanges(newTimeGapHistory);
+						_.forEach(newTimeGapHistory, function(timeGap) {
+							stats.timeGapHistoryForClosestChanges.push(timeGap);
+						});
+						stats.timeGapHistoryForClosestChanges = _.sortByAll(stats.timeGapHistoryForClosestChanges, ['endDate']);
 					}
 				});
 			});
 		});
-		
-		
-		
 		return result;
 	}, {});
+}
+
+function _getNextChangeByOtherAuthorEmail(otherAuthorEmail, changeHistoryForFile, changeIndex) {
+	// find the next change by other author email if it exist
+	var nextChangeByOtherAuthorEmail = null;
+	var curChange = null;
+ 	for (var j = changeIndex; j < changeHistoryForFile.length; j++) {
+ 		curChange = changeHistoryForFile[j];
+ 		if (curChange.authorEmail === otherAuthorEmail) {
+ 			nextChangeByOtherAuthorEmail = curChange;
+ 		}
+ 	}
+ 	return nextChangeByOtherAuthorEmail;
+}
+
+function _getPrevChangeByOtherAuthorEmail(otherAuthorEmail, changeHistoryForFile, changeIndex) {
+	// find the next change by other author email if it exist
+	var prevChangeByOtherAuthorEmail = null;
+	var curChange = null;
+ 	for (var j = changeIndex; j >= 0; j--) {
+ 		curChange = changeHistoryForFile[j];
+ 		if (curChange.authorEmail === otherAuthorEmail) {
+ 			prevChangeByOtherAuthorEmail = curChange;
+ 		}
+ 	}
+ 	return prevChangeByOtherAuthorEmail;
+}
+
+function _getClosestChangeByOtherAuthorEmail(otherAuthorEmail, changeHistoryForFile, changeByAuthorEmailIndex) {
+	var curChangeByAuthorEmail = changeHistoryForFile[changeByAuthorEmailIndex];
+	var prevChangeByOtherAuthorEmail = _getPrevChangeByOtherAuthorEmail(otherAuthorEmail, changeHistoryForFile, changeByAuthorEmailIndex);
+	var nextChangeByOtherAuthorEmail = _getNextChangeByOtherAuthorEmail(otherAuthorEmail, changeHistoryForFile, changeByAuthorEmailIndex);
+	if (prevChangeByOtherAuthorEmail) {
+		if (nextChangeByOtherAuthorEmail) {
+			var prevChangeDuration = math.abs(prevChangeByOtherAuthorEmail.date - curChangeByAuthorEmail.date);
+			var nextChangeDuration = math.abs(nextChangeByOtherAuthorEmail.date - curChangeByAuthorEmail.date);
+			if (prevChangeDuration <= nextChangeDuration) {
+				return prevChangeByOtherAuthorEmail;
+			} else {
+				return nextChangeByOtherAuthorEmail;
+			}
+		} else {
+			return prevChangeByOtherAuthorEmail;
+		}
+	} else if (nextChangeByOtherAuthorEmail) {
+		return nextChangeByOtherAuthorEmail;
+	} else {
+		return null;
+	}
+}
+
+function getTimeGapHistoryForClosestChanges(authorEmail, otherAuthorEmail, changeHistoryForFile) {
+	// this method assumes that changeHistoryForFile is ordered
+	// by the date of the changes in ascending order
+
+	// for each of changes to this file by authorEmail, 
+	// discover the time gap between it and the most recent previous commit by otherAuthorEmail
+	var hasNoClosestChange = false;
+	var timeGapHistoryForClosestChanges = _.reduce(changeHistoryForFile, function(result, change, i) {
+		 if (hasNoClosestChange) {
+		 	return result;
+		 }
+		 if (change.authorEmail === authorEmail) {
+			var closestChangeByOtherAuthorEmail = _getClosestChangeByOtherAuthorEmail(otherAuthorEmail, changeHistoryForFile, i);
+		 	if (closestChangeByOtherAuthorEmail) {
+		 		// record the time gap between the change by fromAuthorEmail
+		 		// and the most recent previous change by toAuthorEmail
+		 		var otherAuthorDate = closestChangeByOtherAuthorEmail.date;
+		 		var authorDate = change.date;
+		 		var timeGap = {
+		 			authorDate: authorDate,
+		 			otherAuthorDate: otherAuthorDate,
+		 			authorEmail: authorEmail,
+		 			otherAuthorEmail: otherAuthorEmail,
+		 			duration: math.abs(otherAuthorDate - authorDate)
+		 		};
+		 		// find the time difference between the change by authorEmail
+		 		// and the most recent previous change by otherAuthorEmail
+		 		result.push(timeGap);  
+		 	} else {
+		 		hasNoClosestChange = true;
+		 	}
+		 }
+		 return result;
+	}, []);
+	return timeGapHistoryForClosestChanges;
+}
+
+function getTotalTimeBetweenClosestChanges(timeGapHistoryForClosestChanges) {
+	return _.reduce(timeGapHistoryForClosestChanges, function(result, timeGap) {
+		result += timeGap.duration;
+		return result; 
+	}, 0);
 }
 
 function getJsonRepoLogUrlsFromRepoNames(repoNames) {
@@ -159,7 +271,6 @@ function showContributionsByFile(contributionsByFile) {
 			authorStatListElement.appendTo(authorListItemElement);
 			authorStatListElement.append('<li>lines added: ' + contribution.totalAddedLines + '</li>');
 			authorStatListElement.append('<li>lines removed: ' + contribution.totalRemovedLines + '</li>');
-			//console.log(contribution);
 		});
 	});
 }
