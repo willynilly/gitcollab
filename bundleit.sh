4d1ced7ae75a43670c6d271a190005ef4bbad032
Will Riley <wanderingwill@gmail.com>
2015-01-30 20:07:40 -0500

    Fixes bundleit script.

--- bundleit.sh
+++ bundleit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-browserify -r lodash -r jquery-browserify -r mathjs -r ./gitcollab.js:gitcollab -o bundle.js -v
+browserify -r lodash -r jquery-browserify -r mathjs -r ./gitcollab.js:gitcollab -o bundle.js
